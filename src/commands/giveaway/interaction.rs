176cdbf47c0a75065835a6cf5da9d5098c2424c5
Fabio Almeida <me@fabioalmeida.dev>
2023-10-11 03:38:02 +0100

    feat: role restrictions

--- src/commands/giveaway/interaction.rs
+++ src/commands/giveaway/interaction.rs
@@ -1,7 +1,7 @@
 use std::time::Duration;
 
 use serenity::{
-    all::{ButtonStyle, ChannelId, MessageId},
+    all::{ButtonStyle, ChannelId, MessageId, RoleId},
     builder::{CreateActionRow, CreateButton, CreateEmbed, EditMessage},
     futures::StreamExt,
 };
@@ -57,6 +57,25 @@ pub async fn new_giveaway_entry_handler(handler: Handler, ctx: CommandContext, g
     // This will run while the giveaway is still active
     while let Some(interaction) = interaction_stream.next().await {
         let interaction_context = InteractionContext::new(ctx.ctx.clone(), &interaction);
+
+        if let Some(restriction) = giveaway.role_restriction {
+            let role = RoleId::new(restriction as u64);
+            if !interaction.member.unwrap().roles.contains(&role) {
+                if let Err(err) = interaction_context
+                    .error_message(ResponseError::ExecutionError(
+                        "You do not have permission to enter this giveaway",
+                        None,
+                    ))
+                    .await
+                {
+                    error!(
+                        "Could not notify user of failed giveaway entry. Failed with error: {:?}",
+                        err
+                    );
+                }
+                continue;
+            }
+        }
 
         match sqlx::query!(
             "SELECT id FROM giveaway_entry WHERE id = $1 AND user_id = $2",
