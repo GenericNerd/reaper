70d9c2e8025c4402d845027183f4fa8daef7e26e
Fabio Almeida <me@fabioalmeida.dev>
2023-10-11 04:29:43 +0100

    feat: giveaway restoration on restart

--- src/events/ready.rs
+++ src/events/ready.rs
@@ -1,10 +1,21 @@
-use serenity::{all::Command, gateway::ActivityData, model::prelude::Ready, prelude::Context};
+use std::sync::{atomic::AtomicBool, Arc};
+
+use serenity::{
+    all::{ChannelId, Command, PartialGuild},
+    gateway::ActivityData,
+    model::prelude::Ready,
+    prelude::Context,
+};
 use tracing::{debug, error, info};
 
 use crate::{
-    commands::get_command_list,
+    commands::{get_command_list, giveaway::interaction::new_giveaway_entry_handler},
     events::expire::{expire_actions, expire_giveaways},
-    models::handler::Handler,
+    models::{
+        command::CommandContext,
+        giveaway::{DatabaseGiveaway, Giveaway},
+        handler::Handler,
+    },
 };
 
 impl Handler {
@@ -19,6 +30,58 @@ impl Handler {
         tokio::spawn(expire_actions(self.clone(), ctx.clone()));
         debug!("Starting giveaway expiration loop");
         tokio::spawn(expire_giveaways(self.clone()));
+
+        match sqlx::query_as!(DatabaseGiveaway, "SELECT * FROM giveaways")
+            .fetch_all(&self.main_database)
+            .await
+        {
+            Ok(giveaways) => {
+                debug!("Adding {} giveaways to giveaway list", giveaways.len());
+                for giveaway in giveaways {
+                    let Ok(channel) = ctx
+                        .http
+                        .get_channel(ChannelId::new(giveaway.channel_id as u64))
+                        .await
+                    else {
+                        error!("Failed to get channel for giveaway {}", giveaway.id);
+                        continue;
+                    };
+                    let Some(guild_id) = channel.guild().map(|guild| guild.guild_id) else {
+                        error!("Failed to get guild for giveaway {}", giveaway.id);
+                        continue;
+                    };
+
+                    let mut temp_guild = guild_id
+                        .to_guild_cached(&ctx.cache)
+                        .map(|guild| PartialGuild::from(guild.clone()));
+                    if temp_guild.is_none() {
+                        temp_guild = if let Ok(guild) = guild_id.to_partial_guild(&ctx.http).await {
+                            Some(guild)
+                        } else {
+                            return;
+                        }
+                    }
+                    let guild = temp_guild.unwrap();
+
+                    let command_context = CommandContext {
+                        ctx: ctx.clone(),
+                        has_responsed: Arc::new(AtomicBool::new(false)),
+                        user_permissions: vec![],
+                        guild,
+                    };
+
+                    tokio::spawn(new_giveaway_entry_handler(
+                        self.clone(),
+                        command_context,
+                        Giveaway::from(giveaway),
+                    ));
+                }
+            }
+            Err(e) => error!(
+                "Failed to get giveaways from database. Failed with error: {}",
+                e
+            ),
+        }
 
         debug!("Adding current commands to slash commands list");
         let mut successful_commands = vec![];
