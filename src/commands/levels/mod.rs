1e0cb2cc65e523eff715aec3f82d2b7593c401cc
Fabio Almeida <me@fabioalmeida.dev>
2024-09-26 02:53:52 +0100

    refactor: register level commands

--- src/commands/levels/mod.rs
+++ src/commands/levels/mod.rs
@@ -1,4 +1,15 @@
+use crate::models::command::Command;
+
 pub mod leaderboard;
 pub mod level;
 pub mod rank;
 pub mod xp;
+
+pub fn get_level_commands() -> Vec<Box<dyn Command>> {
+    vec![
+        Box::new(leaderboard::LeaderboardCommand),
+        Box::new(level::LevelCommand),
+        Box::new(rank::RankCommand),
+        Box::new(xp::XPCommand),
+    ]
+}
--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -4,13 +4,14 @@ pub mod config;
 pub mod giveaway;
 pub mod global;
 pub mod info;
+pub mod levels;
 pub mod moderation;
 pub mod permissions;
 pub mod privacy;
-pub mod levels;
 
 pub fn get_command_list() -> Vec<Box<dyn Command>> {
     let mut commands = moderation::get_moderation_commands();
+    commands.extend(levels::get_level_commands());
     commands.push(Box::new(permissions::PermissionsCommand));
     commands.push(Box::new(giveaway::GiveawayCommand));
     commands.push(Box::new(config::ConfigCommand));
