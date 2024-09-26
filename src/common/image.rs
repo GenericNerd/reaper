5b9e8ad4abb76d8ae07e43f82f7a235d62c4a2ef
Fabio Almeida <me@fabioalmeida.dev>
2024-09-26 04:13:59 +0100

    fix: crash when on a level boundary

--- src/common/image.rs
+++ src/common/image.rs
@@ -157,16 +157,18 @@ impl Handler {
                 .color(colors::WHITE),
         );
 
-        image.add_rect(
-            Rect::new()
-                .size(
-                    ((IMAGE_WIDTH - IMAGE_HEIGHT - 20 - 10) as f32 * progress_to_next_level).round()
-                        as u32,
-                    IMAGE_HEIGHT / 3 - 10,
-                )
-                .position(IMAGE_HEIGHT + 10, IMAGE_HEIGHT - (IMAGE_HEIGHT / 3))
-                .color([235, 151, 109, 255]),
-        );
+        if progress_to_next_level > 0_f32 {
+            image.add_rect(
+                Rect::new()
+                    .size(
+                        ((IMAGE_WIDTH - IMAGE_HEIGHT - 20 - 10) as f32 * progress_to_next_level)
+                            .round() as u32,
+                        IMAGE_HEIGHT / 3 - 10,
+                    )
+                    .position(IMAGE_HEIGHT + 10, IMAGE_HEIGHT - (IMAGE_HEIGHT / 3))
+                    .color([235, 151, 109, 255]),
+            );
+        }
 
         let filename = &format!("{user_id}_{avatar_hash}.png");
         image.save(filename);
